cf828c9b069a13b60e59bb595ffd162180009b22
John Harvey <thegradientvector@gmail.com>
2016-06-24 14:06:56 -0500

    Updated to serve the Otter Slide!

--- Program.cs
+++ Program.cs
@@ -3,8 +3,6 @@ using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.CommandLine;
-using Microsoft.Extensions.Logging;
 
 namespace thegradientvector.com
 {
@@ -12,7 +10,9 @@ namespace thegradientvector.com
     {
         public static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
             var host = new WebHostBuilder()
               .UseConfiguration(config)
               .UseKestrel()
--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,13 @@
 using System;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.CommandLine;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace thegradientvector.com
 {
@@ -9,9 +15,28 @@ namespace thegradientvector.com
   {
     public void Configure(IApplicationBuilder app)
     {
+      app.UseDefaultFiles();
+      app.UseStaticFiles();
+      app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Directory.GetCurrentDirectory()),
+                RequestPath = new PathString("")
+            });
+
+      string html = @"
+      <!doctype HTML>
+      <html>
+      <head></head>
+      <body>
+        <h2>Hello from the otter slide!</h2>
+        <img src='HelloFromTheOtterSlide.jpg' />
+      </body>
+      </html>
+      ";
       app.Run(context =>
       {
-        return context.Response.WriteAsync("Hello from the otter slide!");
+        return context.Response.WriteAsync(html);
       });
     }
   }
