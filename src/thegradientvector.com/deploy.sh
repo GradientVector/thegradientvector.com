d947425f7d71abcd294c7c441b19fd623ca07489
John Harvey <thegradientvector@gmail.com>
2016-06-25 06:31:36 +0000

    Updated the deploy script

--- src/thegradientvector.com/deploy.sh
+++ src/thegradientvector.com/deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+dotnet restore
 dotnet publish
 
 #ssh root@thegradientvector.com "rm -fr /var/aspnet/thegradientvector.com/approot/*; mkdir -p /var/aspnet/thegradientvector.com/approot; chown www-data:www-data /var/aspnet/thegradientvector.com/approot"
@@ -7,9 +8,11 @@ dotnet publish
 #ssh root@thegradientvector.com "chown -R www-data:www-data /var/aspnet/thegradientvector.com/approot/; chmod +x /var/aspnet/thegradientvector.com/approot/run.sh"
 
 rm -r /var/aspnet/thegradientvector.com/approot/
+rm /var/aspnet/thegradientvector.com/kestrel.sock
 mkdir -p /var/aspnet/thegradientvector.com/approot
 cp -r bin/Debug/netcoreapp1.0/publish/* /var/aspnet/thegradientvector.com/approot
 cp run.sh /var/aspnet/thegradientvector.com/approot/
+chmod +x /var/aspnet/thegradientvector.com/approot/run.sh
 chown -R www-data:www-data /var/aspnet/thegradientvector.com/approot
 
 # TODO - maybe comment this out once I figure out how to do it right?
